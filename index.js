48f34db53c93289c9c7def6d394798af39ed7eaa
David <behoiandavid@gmail.com>
2025-03-21 20:54:15 +0200

    first commit, and after commit, i buld new code

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const input2 = document.getElementById('input2');
 const submitBtn = document.getElementById('submit');
 const plusBtn = document.getElementById('plus');
 const minusBtn = document.getElementById('minus');
+const divisionBtn = document.getElementById('division');
+const multiplicationBtn = document.getElementById('multiplication');
 let action = '+';
+
 
 const sum = Number(input1.value) + Number(input2.value);
 resultElement.textContent = sum
 
 // console.log(typeof sum) typeof пароверка типа данных
+
+
 
 plusBtn.onclick = function () {
     action = '+'
@@ -19,10 +24,19 @@ minusBtn.onclick = function () {
     action = '-'
 }
 
+multiplicationBtn.onclick = function () {
+    action = '*'
+}
+
+divisionBtn.onclick = function () {
+    action = '/'
+}
+
 function printResult(result) {
-    if (result < 0) {
+    if (result <= 0) {
         resultElement.style.color = "red";
-    } else {
+    }
+    else {
         resultElement.style.color = "green";
     }
 
@@ -33,22 +47,35 @@ function printResult(result) {
 function computeNumbersWithAction(input1, input2, actionSymbol) {
     const num1 = Number(input1.value)
     const num2 = Number(input2.value)
-    if (actionSymbol == '+') {
+    if (actionSymbol === '+') {
         return num1 + num2
-    } else if (actionSymbol == '-') {
+    } else if (actionSymbol === '-') {
         return num1 - num2
+    } else if (actionSymbol === '*') {
+        return num1 * num2
+    } else if (actionSymbol === '/') {
+        return num1 / num2
     }
-
 }
 
+
 submitBtn.onclick = function () {
-    const result = computeNumbersWithAction(input1, input2, action)
-    printResult(result)
-    // if (action == '+') {
-    //     const sum = Number(input1.value) + Number(input2.value);
-    //     printResult(sum)
-    // } else if (action == '-') {
-    //     const sum = Number(input1.value) - Number(input2.value);
-    //     printResult(sum)
-    // }
+    const result = computeNumbersWithAction(input1, input2, action,)
+    if (action == '+') {
+        const sum = Number(input1.value) + Number(input2.value);
+        printResult(sum)
+    } else if (action == '-') {
+        const sum = Number(input1.value) - Number(input2.value);
+        printResult(sum)
+    } else if (action == '*') {
+        const sum = Number(input1.value) * Number(input2.value);
+        printResult(sum)
+    } else if (action == '/') {
+        const sum = Number(input1.value) / Number(input2.value);
+        printResult(sum)
+        if (Number(input2.value) === 0) {
+            printResult("Erorr")
+        }
+        return
+    }
 }
