665db48eb71916632e262ccf98156ecfd5102eae
David <behoiandavid@gmail.com>
2025-03-21 03:13:44 +0200

    first commit, and after commit, i buld new code

---
+++ index.js
@@ -0,0 +1,54 @@
+const resultElement = document.getElementById('result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const submitBtn = document.getElementById('submit');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+let action = '+';
+
+const sum = Number(input1.value) + Number(input2.value);
+resultElement.textContent = sum
+
+// console.log(typeof sum) typeof пароверка типа данных
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = "red";
+    } else {
+        resultElement.style.color = "green";
+    }
+
+    resultElement.textContent = result;
+}
+
+
+function computeNumbersWithAction(input1, input2, actionSymbol) {
+    const num1 = Number(input1.value)
+    const num2 = Number(input2.value)
+    if (actionSymbol == '+') {
+        return num1 + num2
+    } else if (actionSymbol == '-') {
+        return num1 - num2
+    }
+
+}
+
+submitBtn.onclick = function () {
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
+    // if (action == '+') {
+    //     const sum = Number(input1.value) + Number(input2.value);
+    //     printResult(sum)
+    // } else if (action == '-') {
+    //     const sum = Number(input1.value) - Number(input2.value);
+    //     printResult(sum)
+    // }
+}
